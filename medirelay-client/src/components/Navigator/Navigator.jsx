f0dbb0411f24f4275fa80134dcf1cdb5d282b854
Pierre Goujet <pierre.goujet.etu@univ-lille.fr>
2024-10-08 11:05:37 +0200

    Page de login sans la logique de connexion

--- medirelay-client/src/components/Navigator/Navigator.jsx
+++ medirelay-client/src/components/Navigator/Navigator.jsx
@@ -1,10 +1,16 @@
 import { Route, Routes} from 'react-router-dom';
 import Home from '../../pages/Home/Home';
+import LoginDocteur from '../../pages/Login/log-doc/Login.doc';
+import LoginPatient from '../../pages/Login/log-patient/Login.patient';
+import LoginPharma from '../../pages/Login/log-pharma/Login.pharma';
 
 const Navigator = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login-doc" element={<LoginDocteur />} />
+            <Route path="/login-patient" element={<LoginPatient />} />
+            <Route path="/login-pharma" element={<LoginPharma />} />
         </Routes>
     );
 };
---
+++ medirelay-client/src/pages/Login/log-patient/Login.patient.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import background from '../../../assets/img/background-home.png';
+import docteurs from '../../../assets/img/docteurs.png';
+import '../Login.css';
+const LoginDocteur = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Ajoutez ici la logique de connexion avec l'email et le mot de passe
+    };
+
+    return (
+        <>
+        <div className='container-img-home'>
+            <img src={background} alt="" className='background-home' />
+            <img src={docteurs} alt="" className='docteurs' />
+        </div>
+        <div className='container-form-login'>
+                <h2>Bonjour Madame/Monsieur, connectez-vous !</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className='container-input'>
+                        <label htmlFor="email">Email:</label>
+                        <input
+                            type="email"
+                            id="email"
+                            value={email}
+                            onChange={handleEmailChange} />
+                    </div>
+                    <div className='container-input'>
+                        <label htmlFor="password">Mot de passe:</label>
+                        <input
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={handlePasswordChange} />
+                    </div>
+                    <button type="submit" className='button-form-login'>Se connecter</button>
+                </form>
+            </div>
+        </>
+    );
+};
+
+export default LoginDocteur;
